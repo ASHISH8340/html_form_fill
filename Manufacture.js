75bd3e116f066ca0eea62bb35beaf5857a0954c0
ashishmourya8340@gmail.com <ashishmourya8340@gmail.com>
2023-06-27 15:52:40 +0530

    assignment2

---
+++ Manufacture.js
@@ -0,0 +1,7 @@
+
+const manufacturers = ["Sony", "Airtel", "Jio", "Nokia", "Samsung"];
+
+
+for (let i = 0; i < manufacturers.length; i++) {
+  console.log(manufacturers[i]);
+}
---
+++ table.js
@@ -0,0 +1,6 @@
+const number = 2;
+
+for (let i = 1; i <= 10; i++) {
+  const result = number * i;
+  console.log(`${number} * ${i} = ${result}`);
+}
